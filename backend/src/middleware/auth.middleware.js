172951f0e20c5b3984cf09fbb4590fb0ec269f4b
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2025-03-18 16:33:28 +0330

    refactor: improve error handling in auth middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,29 +3,29 @@ import User from "../models/user.model.js";
 
 export const auth = async (req, res, next) =>{
    try {
+      const token = req.cookies.jwt;
+      if (!token) return res.status(401).json({message: "Unauthorized - No Token Provided"})
       
-   const token = req.cookies.jwt;
+      let decoded;
+      try {
+         decoded = jwt.verify(token , process.env.JWT_SECRET);
+      } catch (error) {
+         return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+      }
 
-   if (!token) {
-      return res.status(401).json({ message: "Unauthorized - No Token Provided" });
-   }
-   
-   const decoded = jwt.verify(token,process.env.JWT_SECRET);
-   
-   if (!decoded || !decoded.userId ) {
-      return res.status(401).json({ message: "Unauthorized - Invalid Token" });
-   }
+      if (!decoded?.userId) {
+         return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+      }
 
-   const user = await User.findById(decoded.userId).select('-password');
+      const user = await User.findById(decoded.userId).select("-password");
+      if (!user) return res.status(404).json({ message: "User not found" });
 
-   if (!user) {
-      return res.status(404).json({ message: "User not found" });
-   }
+      req.user = user;
+      next();
 
-   req.user = user;
-   next();
    } catch (error) {
-      console.log(error.message);
+      console.error("Authentication Error:", error.message);
+      return res.status(500).json({ message: "Internal Server Error" });
        
    }
 }
