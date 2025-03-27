d90b8441f57cb8b327d144cf8ad1bb1c424e5a6c
Rastin Badpeyma <rastinbadpeyma@gmail.com>
2025-03-27 14:19:49 +0330

    protected route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,10 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv"
-import {mongoConnect} from "./lib/db.js"
+import {connectDB} from "./lib/db.js"
 import cookieParser from 'cookie-parser';
-
+import cors from "cors";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
@@ -12,11 +13,15 @@ const PORT = process.env.PORT;
 app.use(cookieParser());
 
 app.use(express.json());
-app.use('/api/auth' , authRoutes)
-
+app.use('/api/auth' , authRoutes);
+app.use('/api/messages' , messageRoutes);
+app.use(cors({
+   origin: "http://localhost:5173",
+   credentials: true
+}))
 
 
 app.listen(PORT , ()=> {
    console.log(`server is running on PORT:${PORT}`);
-   mongoConnect();
+   connectDB();
 })
